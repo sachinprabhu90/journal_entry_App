ee78d5f588d8d205e748a0fa5b062c342843f978
Sachin Prabhu <39897443+sachinprabhu90@users.noreply.github.com>
2020-04-06 21:50:15 +0530

    UI is much better now

--- journal.py
+++ journal.py
@@ -11,79 +11,78 @@ logging.disable(level=logging.CRITICAL)
 class Journal():
     def __init__(self, master):
         master.title('My Journal')
-        master.configure(background = '#ADABC3')
+        master.configure(background = '#253042')
+        master.resizable(False , False)
 
         # TODO
         # add checklist
 
         style = ttk.Style()
-        style.configure('TEntry', foreground='#354160', disabledfieldbackground = '#ADABC3',
-                        fieldbackground='#ADABC3', font=('Arial', 20))
-        style.configure('TButton', foreground='#354160',
-                        background='#ADABC3', font=('Arial', 12))
-        style.configure('TLabel', background = '#ADABC3',foreground='#354160', font=('Arial', 12))
+        style.configure('TEntry', foreground='white',
+                        fieldbackground='#253042', font=('Arial', 20))
+        # style.map("TEntry",fieldbackground=[("active", "yellow"), ("disabled", "red")])
+        style.configure('TButton', foreground='white',
+                        background='#253042', font=('Arial', 12))
+        style.configure('main.TLabel', background = '#253042',foreground='white', font=('Arial', 12))
 
-        ttk.Label(master, text='Date & Time: ').grid(
-            row=0, column=0, padx=2, pady=3)
-        self.datetime_entry = ttk.Entry(
-            master, state=DISABLED, style='entry.TEntry', width=100)
-        self.datetime_entry.grid(row=0, column=1, padx=2, pady=3)
+        # ttk.Label(master, text='Date & Time: ', style = 'main.TLabel').grid(
+        #     row=0, column=0, padx=2, pady=3)
+        self.datetime_entry = ttk.Entry(master, state='read_only',  width=100)
+        # self.datetime_entry.grid(row=0, column=1, padx=2, pady=3)
         self.refresh_button = ttk.Button(
             master, text='Refresh', command=lambda: self.refresh())
-        self.refresh_button.grid(row=0, column=2, padx=2, pady=3)
+        # self.refresh_button.grid(row=0, column=2, padx=2, pady=3)
 
-        ttk.Label(master, text='Location: ').grid(
+        ttk.Label(master, text='Location: ', style='main.TLabel').grid(
             row=1, column=0, padx=2, pady=3)
-        self.location_entry = ttk.Entry(
-            master, style='entry.TEntry', width=100)
+        self.location_entry = ttk.Entry(master, width=100)
         self.location_entry.grid(row=1, column=1, padx=2, pady=3)
-        self.refresh_location_button = ttk.Button(
-            master, text='Refresh', command=lambda: self.refresh_location())
+        self.refresh_location_button = HoverButton(master,text="Refresh", 
+                                        background='#253042', 
+                                        foreground='white', 
+                                        command=lambda : self.refresh_location())
         self.refresh_location_button.grid(row=1, column=2,  padx=2, pady=3)
 
-        # adding tabs above text
-        tab = ttk.Notebook(master)
-        tab.grid(row=2,column = 0,columnspan = 50,rowspan = 49,sticky='NESW')
-        tab1 = ttk.Frame(tab,style='My.TFrame')
-        tab.add(tab1,text='Tab1')
+        # self.text_frame = ttk.Frame(master, width=100, height=100)
+        # self.text_frame.grid(row=2, column=0, columnspan=2)
 
- 
-        nb = ttk.Notebook(tab1)
-        page1 = ttk.Frame(nb)
-        nb.add(page1,text='Scanning')
+        self.text_entry = Text(
+            master, height=30, wrap='word', background = '#373B44',foreground='white', font=('Time New Roman', 15))
+        self.text_entry.grid(row=2, column=0, columnspan=3,
+                             sticky='NESW', padx=2, pady=5)
+    
 
-        rows=0
-        while rows < 50:
-            tab1.rowconfigure(rows,weight=1)
-            tab1.columnconfigure(rows,weight=1)
-            rows+=1
+        self.submit_button = HoverButton(master,text="Submit", 
+                                        background='#253042', 
+                                        foreground='white', 
+                                        command=lambda : self.submit())
 
-        page2 = ttk.Frame(nb)
-        nb.add(page2,text='Imagine')
-        
-        page3 = ttk.Frame(nb)
-        nb.add(page3,text='Impurity')
-        
-        page4 = ttk.Frame(nb)
-        nb.add(page4,text='Marking')
-
-        # self.text_entry = Text(
-        #     master, height=30, background = '#ADABC3',foreground='#354160', font=('Time New Roman', 15))
-        # self.text_entry.grid(row=2, column=0, columnspan=3,
-        #                      sticky='NESW', padx=2, pady=5)
-
-        self.submit_button = ttk.Button(
-            master, text='Submit', command=lambda: self.submit())
         self.submit_button.grid(
-            row=3, column=0, columnspan=3, sticky='NESW', padx=2, pady=5)
+            row=3, column=0, sticky='NE', padx=2, pady=5)
+
+
+        self.clear_button = HoverButton(master,text="Clear", 
+                                        background='#253042', 
+                                        foreground='white', 
+                                        command=lambda : self.clear())
+        self.clear_button.grid(row=3, column=1, sticky='NW', padx=10, pady=5)
+
+        self.about_button = HoverButton(master,text="About", 
+                                        background='#253042', 
+                                        foreground='white', 
+                                        command=lambda : self.show_help())
+        self.about_button.grid(row=3, column=2, sticky='NE', padx=10, pady=5)
 
         self.refresh()
-        self.refresh_location()
+        # self.refresh_location()
 
         # keyboard bindings
-        master.bind('<Return>', lambda e: self.submit())
+        master.bind('<Control-Return>', lambda e: self.submit())
+        self.text_entry.focus()
 
     def refresh(self):
+        self.text_entry.delete(1.0, 'end')
+        self.text_entry.insert(1.0, 'Journal Entry Here!')
         self.datetime_entry.config(state=NORMAL)
         self.datetime_entry.delete(0, 'end')
         self.datetime_entry.insert(
@@ -98,10 +97,6 @@ class Journal():
             self.location_entry.insert(0,
                                        f'{g.city} lat:{g.latlng[0]} lng:{g.latlng[1]}'
                                        )
-            messagebox.showinfo(title='location',
-                                message=f'Location Refreshed\nCity:{g.city}\nlatlang:{g.latlng}'
-                                )
-
         else:
             self.location_entry.insert(0, 'Enter location')
 
@@ -133,6 +128,24 @@ class Journal():
         self.location_entry.delete(0, 'end')
         self.text_entry.delete(1.0, 'end')
 
+    def show_help(self):
+        messagebox.showinfo(title='Help',
+        message='My Journal helps you to arrange your journal entries along with timestamp and\
+                location. Once you submit, new journal entry will be appended to the file selected.\
+                \n\nLocation is fetched based on the IP.')
+
+class HoverButton(Button):
+    def __init__(self, master, **kw):
+        Button.__init__(self,master=master,**kw)
+        self.defaultBackground = self["background"]
+        self.bind("<Enter>", self.on_enter)
+        self.bind("<Leave>", self.on_leave)
+
+    def on_enter(self, e):
+        self['background'] = self['activebackground']
+
+    def on_leave(self, e):
+        self['background'] = self.defaultBackground
 
 def main():
     root = Tk()
