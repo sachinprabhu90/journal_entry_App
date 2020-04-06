a6bbc2ad8fb539a340b1f452e5b8e2b221417289
Sachin Prabhu <39897443+sachinprabhu90@users.noreply.github.com>
2020-04-06 21:43:17 +0530

    UI is much better now

--- journal.py
+++ journal.py
@@ -5,22 +5,23 @@ import geocoder
 import os
 import logging
 logging.basicConfig(level=logging.DEBUG)
-# logging.disable(level=logging.CRITICAL)
+logging.disable(level=logging.CRITICAL)
 
 
 class Journal():
     def __init__(self, master):
         master.title('My Journal')
+        master.configure(background = '#ADABC3')
 
         # TODO
         # add checklist
 
         style = ttk.Style()
-        style.configure('TEntry', foreground='blue',
-                        background='grey', font=('Arial', 20))
-        style.configure('TButton', foreground='blue',
-                        background='LightBlue3', font=('Arial', 12))
-        style.configure('TLabel', foreground='blue', font=('Arial', 12))
+        style.configure('TEntry', foreground='#354160', disabledfieldbackground = '#ADABC3',
+                        fieldbackground='#ADABC3', font=('Arial', 20))
+        style.configure('TButton', foreground='#354160',
+                        background='#ADABC3', font=('Arial', 12))
+        style.configure('TLabel', background = '#ADABC3',foreground='#354160', font=('Arial', 12))
 
         ttk.Label(master, text='Date & Time: ').grid(
             row=0, column=0, padx=2, pady=3)
@@ -33,19 +34,43 @@ class Journal():
 
         ttk.Label(master, text='Location: ').grid(
             row=1, column=0, padx=2, pady=3)
-        self.location_entry = ttk.Entry(master, style='entry.TEntry', width=100)
+        self.location_entry = ttk.Entry(
+            master, style='entry.TEntry', width=100)
         self.location_entry.grid(row=1, column=1, padx=2, pady=3)
         self.refresh_location_button = ttk.Button(
             master, text='Refresh', command=lambda: self.refresh_location())
         self.refresh_location_button.grid(row=1, column=2,  padx=2, pady=3)
 
-        # self.text_frame = ttk.Frame(master, width=100, height=100)
-        # self.text_frame.grid(row=2, column=0, columnspan=2)
+        # adding tabs above text
+        tab = ttk.Notebook(master)
+        tab.grid(row=2,column = 0,columnspan = 50,rowspan = 49,sticky='NESW')
+        tab1 = ttk.Frame(tab,style='My.TFrame')
+        tab.add(tab1,text='Tab1')
 
-        self.text_entry = Text(
-            master, height=30, foreground='blue', font=('Time New Roman', 15))
-        self.text_entry.grid(row=2, column=0, columnspan=3,
-                             sticky='NESW', padx=2, pady=5)
+ 
+        nb = ttk.Notebook(tab1)
+        page1 = ttk.Frame(nb)
+        nb.add(page1,text='Scanning')
+
+        rows=0
+        while rows < 50:
+            tab1.rowconfigure(rows,weight=1)
+            tab1.columnconfigure(rows,weight=1)
+            rows+=1
+
+        page2 = ttk.Frame(nb)
+        nb.add(page2,text='Imagine')
+        
+        page3 = ttk.Frame(nb)
+        nb.add(page3,text='Impurity')
+        
+        page4 = ttk.Frame(nb)
+        nb.add(page4,text='Marking')
+
+        # self.text_entry = Text(
+        #     master, height=30, background = '#ADABC3',foreground='#354160', font=('Time New Roman', 15))
+        # self.text_entry.grid(row=2, column=0, columnspan=3,
+        #                      sticky='NESW', padx=2, pady=5)
 
         self.submit_button = ttk.Button(
             master, text='Submit', command=lambda: self.submit())
@@ -55,7 +80,7 @@ class Journal():
         self.refresh()
         self.refresh_location()
 
-        #keyboard bindings
+        # keyboard bindings
         master.bind('<Return>', lambda e: self.submit())
 
     def refresh(self):
@@ -70,7 +95,13 @@ class Journal():
         logging.debug(print(g))
         self.location_entry.delete(0, 'end')
         if g:
-            self.location_entry.insert(0, f'{g.city} lat:{g.latlng[0]} lng:{g.latlng[1]}')
+            self.location_entry.insert(0,
+                                       f'{g.city} lat:{g.latlng[0]} lng:{g.latlng[1]}'
+                                       )
+            messagebox.showinfo(title='location',
+                                message=f'Location Refreshed\nCity:{g.city}\nlatlang:{g.latlng}'
+                                )
+
         else:
             self.location_entry.insert(0, 'Enter location')
 
@@ -81,7 +112,8 @@ class Journal():
         self.clear()
 
     def save_to_file(self):
-        messagebox.showinfo(title='Info', message='If you already have a journal entry file select .txt file and your journal entry will be appended\nIgnore the system warning')
+        # messagebox.showinfo(
+        # title='Info', message='If you already have a journal entry file select .txt file and your journal entry will be appended\nIgnore the system warning')
         self.filename = filedialog.asksaveasfilename(
             filetypes=(("Text File", "*.txt"),))
         logging.debug(type(self.filename))
